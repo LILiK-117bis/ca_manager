c5d711816ceb54d75323136ab8d471d0e92ab24e
Edoardo Putti <edoardo.putti@gmail.com>
2016-08-20 01:06:13 +0200

    move paths to separate namespace

---
+++ paths.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+MANAGER_PATH = "/home/edoput/ca_manager/private"
+REQUESTS_PATH = "/home/edoput/ca_manager/requests"
+OUTPUT_PATH = "/home/edoput/ca_manager/outputs"
+RESULTS_PATH = "/home/edoput/ca_manager/results"
+
+all = [
+        MANAGER_PATH,
+        RESULTS_PATH,
+        OUTPUT_PATH,
+        RESULTS_PATH,
+        ]
