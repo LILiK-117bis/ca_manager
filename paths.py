8c74057b3e6dc76d6ea433cdfd98bc80dd46a945
Edoardo Putti <edoardo.putti@gmail.com>
2016-09-10 13:20:48 +0200

    correct path to use production values

--- paths.py
+++ paths.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-MANAGER_PATH = "/home/edoput/ca_manager/private"
-REQUESTS_PATH = "/home/edoput/ca_manager/requests"
-OUTPUT_PATH = "/home/edoput/ca_manager/outputs"
-RESULTS_PATH = "/home/edoput/ca_manager/results"
+MANAGER_PATH = "/var/lib/ca_manager/private"
+REQUESTS_PATH = "/var/lib/ca_manager/requests"
+OUTPUT_PATH = "/var/lib/ca_manager/outputs"
+RESULTS_PATH = "/var/lib/ca_manager/results"
 
 all = [
         MANAGER_PATH,
