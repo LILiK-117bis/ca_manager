39885beb0d9bb6e165158473f57b9a6ce4a1d1f8
Edoardo Putti <edoardo.putti@gmail.com>
2017-09-02 18:27:16 +0200

    add openssl configuration file to python package

--- setup.py
+++ setup.py
@@ -21,5 +21,10 @@ setup(
         'bin/ca-server',
         'bin/ca-shell',
     ],
+    package_data={
+        'ca_manager.models': [
+            'openssl-config/*.cnf',
+        ,
+    },
     zip_safe=False
 )
