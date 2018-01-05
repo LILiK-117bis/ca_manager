ed02965f5b10a96d425fe5fe04e6ad844b931886
Edoardo Putti <edoardo.putti@gmail.com>
2018-01-05 17:46:56 +0100

    fix whitespace issues with completion function

--- ca_manager/shell.py
+++ ca_manager/shell.py
@@ -244,14 +244,14 @@ class CAManagerShell(cmd.Cmd):
             return [request.req_id for i, request in enumerate(self.ca_manager.request) if request.req_id.startswith(text) and request.req_id not in argv[1:]]
 
     def common_complete_ca(self, text, line, begidx, endidx, check_argc=2):
-        argc = len(("%send"%line).split())
+        argc = len(("%send" % line).split())
         if check_argc is None or argc == check_argc:
-            return [ca_item.ca_id for i, ca_item  in enumerate(self.ca_manager.ca) if ca_item.ca_id.startswith(text)]
+            return [ca_item.ca_id for i, ca_item in enumerate(self.ca_manager.ca) if ca_item.ca_id.startswith(text)]
 
     def common_complete_certificate(self, text, line, begidx, endidx, check_argc=2):
-        argc = len(("%send"%line).split())
+        argc = len(("%send" % line).split())
         if check_argc is None or argc == check_argc:
-            return [certificate.cert_id for i, certificate  in enumerate(self.ca_manager.certificate) if certificate.cert_id.startswith(text)]
+            return [certificate.cert_id for i, certificate in enumerate(self.ca_manager.certificate) if certificate.cert_id.startswith(text)]
 
     def complete_drop_request(self, text, line, begidx, endidx):
         return self.common_complete_request(text, line, begidx, endidx, None)
