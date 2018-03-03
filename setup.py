1b1dffa17d0a40b909ed22d2185c042ce92cade9
Edoardo Putti <edoardo.putti@gmail.com>
2018-03-03 18:54:19 +0100

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ca_manager',
-    version='0.1',
+    version='0.3',
     description='shell interface for certification authority management',
     author='LILiK',
     url='https://github.com/LILiK-117bis/ca_manager',
